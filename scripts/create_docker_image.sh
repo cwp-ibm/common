b865c7488d004aab44cdee2f3a1a368e0d922e22
ehood <ehood.katz@il.ibm.com>
2020-10-25 18:14:53 +0200

    Refactor logic for creating executable wrapped Docker image.

--- scripts/create_docker_image.sh
+++ scripts/create_docker_image.sh
@@ -1,11 +1,88 @@
 #!/bin/bash
-DEFAULT_OS=ubuntu
-DEFAULT_OS_VERSION=20.04
+echo ""
+DEFAULT_BASE_IMAGE=ubuntu:20.04
+BASE_IMAGE=${DEFAULT_BASE_IMAGE}
+CONTEXT=
+EXECUTABLE=
+OVERRIDE=false
 
-MALWARE=$1
-OS=${2:-${DEFAULT_OS}}
-OS_VERSION=${3:-$DEFAULT_OS_VERSION}
+function usage()
+{
+    echo -n "
+    Create Docker image to run executable
 
-IMAGE_TAG="${MALWARE}":"${OS}"_"${OS_VERSION}"
+    Usage:
+      ./create_docker_image.sh --context <CONTEXT> --executable <EXECUTABLE> [--base-image <BASE IMAGE>] [--force]
 
-docker build --build-arg OS="${OS}" --build-arg VERSION="${OS_VERSION}" --build-arg EXECUTABLE="${MALWARE}" --tag "${IMAGE_TAG}" .
+    Options:
+      --help                        Show this screen
+      --context <CONTEXT>           Set the docker context 
+      --executable <EXECUTABLE>     Set the executable to be run 
+      --base-image <BASE IMAGE>     Set the base image in which the <EXECUTABLE> will be run in.
+                                    If not defined defaults to ubuntu:20.04
+      --force                       override previous generated Dockerfile in context
+    "
+    echo
+}
+
+function parse_args()
+{
+    if [[ "$#" -eq 0 ]]; then
+        usage
+        exit 1
+    fi
+
+    while (( "$#" )); do
+        case "$1" in
+            --context)
+            CONTEXT="$2"
+            if [ ! -d "$CONTEXT" ]; then
+                echo "Error: --context value does not exist or is an invalid directory" >&2
+                usage
+                exit 1
+            fi
+            shift 2
+            ;;
+            --executable)
+            EXECUTABLE="$2"
+            shift 2
+            ;;
+            --base-image)
+            BASE_IMAGE="$2"
+            shift 2
+            ;;
+            --force)
+            OVERRIDE=true
+            shift 
+            ;;
+            --help)
+            usage
+            exit 0
+            ;;
+            --) # end argument parsing
+            shift
+            break
+            ;;
+            -*|--*) # unsupported flags
+            echo "Error: Unsupported flag $1" >&2
+            exit 1
+            ;;
+            *) # preserve positional arguments
+            PARAMS="$PARAMS $1"
+            shift
+            ;;
+        esac
+    done
+}
+
+parse_args "$@"
+
+IMAGE_TAG="$(basename ${EXECUTABLE})":"${BASE_IMAGE/:/_}"
+
+if [[ "${OVERRIDE}" == true ]]; then
+    echo "Overriding previous Dockerfile in context"
+    sed -n "s/BASE_IMAGE/${BASE_IMAGE}/g;s/EXECUTABLE/${EXECUTABLE}/g;w Dockerfile" Dockerfile.template
+    mv Dockerfile "${CONTEXT}"
+fi
+
+docker build --tag "${IMAGE_TAG}" "${CONTEXT}"
