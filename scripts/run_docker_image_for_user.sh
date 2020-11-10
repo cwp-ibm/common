bf99840d341ef6e9d409aaaad39107127a812e3d
ehood <ehood.katz@il.ibm.com>
2020-11-10 18:50:46 +0200

    Define script for running docker image as specific user

---
+++ scripts/run_docker_image_for_user.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+function usage()
+{
+    echo -n "
+    Start Sysflow monitor. 
+
+    Usage:
+      ./run_docker_image.sh --user <USER> --docker-image <DOCKER-IMAGE> --duration <DURATION>
+
+    Options:
+      --help                            Show this screen
+      --user <USER>                     Run docker as USER
+      --docker-image <DOCKER-IMAGE>     The Docker image to run 
+      --duration <DURATION>	            The duration in seconds before stoping the docker container
+    "
+    echo
+}
+
+function parse_args()
+{
+    if [[ "$#" -eq 0 ]]; then
+        usage
+        exit 1
+    fi
+
+    while (( "$#" )); do
+        case "$1" in
+            --user)
+            USER="$2"
+            shift 2
+            ;;
+            --docker-image)
+            DOCKER_IMAGE="$2"
+            shift 2
+            ;;
+			--duration)
+            DURATION="$2"
+            shift 2
+            ;;
+            --help)
+            usage
+            exit 0
+            ;;
+            --) # end argument parsing
+            shift
+            break
+            ;;
+            -*|--*) # unsupported flags
+            echo "Error: Unsupported flag $1" >&2
+            exit 1
+            ;;
+            *) # preserve positional arguments
+            PARAMS="$PARAMS $1"
+            shift
+            ;;
+        esac
+    done
+}
+
+parse_args "$@"
+
+source ./utils.sh
+
+generate_container_name $DOCKER_IMAGE
+
+runuser -l $USER -c "docker run --rm --stop-timeout $DURATION --name $CONTAINER_NAME $DOCKER_IMAGE"
