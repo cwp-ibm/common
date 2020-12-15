5f1c7949f7467f9998a16354e8a7536793c64926
ehood <ehood.katz@il.ibm.com>
2020-12-15 18:18:13 +0200

    Delete the individual scripts and create single script for analyzing malware.

--- scripts/utils.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export SYSFLOW_CONTAINER_OUTPUT_FILENAME=sysflow.log
-export SYSFLOW_CONTAINER_OUTPUT_PATH=/mnt/data
-
-function generate_container_name() {
-    local DOCKER_IMAGE=$1
-    CONTAINER_NAME=${DOCKER_IMAGE//[:.]/_}
-}
