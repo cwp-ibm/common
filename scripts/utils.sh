dcbeb9f93c2d839d1c3b450038ce3521bb2a451e
ehood <ehood.katz@il.ibm.com>
2021-01-04 11:51:15 +0200

    Define utils script containing common functionality

---
+++ scripts/utils.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+function exit_on_error() {
+  exit_code=$1
+  last_command=${@:2}
+  if [ $exit_code -ne 0 ]; then
+    >&2 echo "\"${last_command}\" command failed with exit code ${exit_code}."
+    exit $exit_code
+  fi
+}
+
+function create_account() {
+  local __UNIQUE_ID=`shuf --input-range=100000-999999 --head-count=1`
+  local __USER_NAME="malware-docker-${__UNIQUE_ID}"
+  local __USER_EXISTS=$(getent passwd ${__USER_NAME})
+  if [[ -z ${__USER_EXISTS} ]]; # Create User if does not exists
+  then
+      echo "*** Creating User Account ${__USER_NAME} ***"
+      adduser --disabled-password --gecos "${__USER_NAME}" ${__USER_NAME}
+      exit_on_error $? !!
+      echo "*** Adding user account ${__USER_NAME} to docker group ***"
+      adduser ${__USER_NAME} docker
+      exit_on_error $? !!
+      echo "*** Removing password from user account ${__USER_NAME} ***"
+      passwd --delete ${__USER_NAME}
+      exit_on_error $? !!
+  fi
+  if [[ -n $1 ]];
+  then
+    local __RESULTVAR=$1
+    eval $__RESULTVAR="'$__USER_NAME'"
+  fi
+}
+
+function delete_account() {
+  local __USER_EXISTS=$(getent passwd $1)
+  if [[ -n ${__USER_EXISTS} ]]; # Delete User only if exists
+  then
+    echo "*** Deleting Account $1"
+    deluser $1 --remove-home
+    exit_on_error $? !!
+  fi
+}
+
+set -o history -o histexpand
+
