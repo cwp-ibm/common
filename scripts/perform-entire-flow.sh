509d5e5f0a09df2675807498c6cac1143239fda9
ehood <ehood.katz@il.ibm.com>
2021-02-21 23:54:23 +0200

    Update entire flow script

--- scripts/perform-entire-flow.sh
+++ scripts/perform-entire-flow.sh
@@ -4,4 +4,4 @@ SCRIPT_FOLDER=$(dirname $(readlink -f "$0"))
 
 ${SCRIPT_FOLDER}/download-malware-from-vt-handler.sh 
 ${SCRIPT_FOLDER}/perform-pending-analysis.sh
-${SCRIPT_FOLDER}/upload-pending.sh
+${SCRIPT_FOLDER}/upload-pending-sysflow-analysis.sh
